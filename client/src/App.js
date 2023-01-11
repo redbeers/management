26fa523a01e70c5558456ad11be13d234d4ef166
박상빈 <tkdqls21@exotech.kr>
2023-01-12 00:14:07 +0900

    view

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import Customer from './components/Customer';
+import Customer from './components/Customer'
 import './App.css';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -7,50 +7,52 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
-import { mergeClasses } from '@material-ui/styles';
+
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginnTop: theme.spacing.unit * 3,
-    overvlowX: "auth"
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
-})
-const customers =[
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '홍길동',
-  'birthday': '930827',
-  'gender': '남자',
-  'job': '직장인'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '이순신',
-  'birthday': '930827',
-  'gender': '여자',
-  'job': '직장인'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '이인겸',
-  'birthday': '930827',
-  'gender': '남자',
-  'job': '직장인'
-},
-]
+});
+
 class App extends Component {
+
+  state = {
+    customers: "",
+    completed: 0
+  }
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <Paper classNmae = {mergeClasses.root}>
-        <Table calssName={mergeClasses.table}>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
           <TableHead>
             <TableRow>
               <TableCell>번호</TableCell>
@@ -62,12 +64,19 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+            {this.state.customers ? this.state.customers.map(c => {
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+            }) :
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
-    )
+    );
   }
 }
-
 export default withStyles(styles)(App);
